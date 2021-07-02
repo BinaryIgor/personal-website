6da85257200c6768cad41f9fa4c8f79a844cf662
Igor <iroztropinski@gmail.com>
2021-07-02 18:43:12 +0200

    Collapsible idea

--- js/component/components.js
+++ js/component/components.js
@@ -1,8 +1,46 @@
-export function collapsible(text, nestedHtml, id) {
-    return `<div id="${id}" class="collapsible">
-        <h1>${text}</h1>
-        <div class="hidden"> 
-            ${nestedHtml}
-        </div>
-    </div>`
+const EXPANDED_CLASS = "expanded";
+const HIDDEN_CLASS = "hidden";
+
+const EXPAND = "+";
+const HIDE = "-";
+
+
+export function content(body) {
+    return `<div class="content-container">
+                <div class="content fade-in">
+                    ${body}
+                </div>
+            </div>`
+}
+
+export function collapsible(title, body) {
+    return `<section>
+                <div class="collapsible-container">
+                    <h1>${title}</h1> 
+                    <span>${EXPAND}</span>
+                </div>
+                <div class="hidden">
+                    ${body}
+                </div>
+            </section>`;
+}
+
+
+export function initAllCollapsibles() {
+    document.querySelectorAll(".collapsible-container").forEach(c => {
+        const icon = c.querySelector("span");
+        const content = c.nextElementSibling;
+        c.onclick = e => {
+            e.stopPropagation();
+
+            c.parentElement.classList.toggle(EXPANDED_CLASS);
+            content.classList.toggle(HIDDEN_CLASS);
+
+            if (icon.textContent == EXPAND) {
+                icon.textContent = HIDE;
+            } else {
+                icon.textContent = EXPAND;
+            }
+        }
+    });
 }
