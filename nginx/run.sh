433e5f11e3a50eb62f3a713c7d2d32569860ea3a
Igor <iroztropinski@gmail.com>
2021-07-10 10:39:32 +0200

    Setting up http2

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,4 +3,4 @@
 site_dir=${SITE_DIR:-"/home/igor/ws/sites/iroztropinski/site"}
 
 docker build -t custom-nginx .
-docker run -v "${site_dir}:/usr/share/nginx/html:ro"  -p 8080:80 custom-nginx
+docker run -v "${site_dir}:/usr/share/nginx/html:ro"  -p 443:443 -p 8080:80 custom-nginx
